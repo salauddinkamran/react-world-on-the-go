5369a7b7b50571cdd214b86adc640bf5c3386a62
kamran <mma744237@gmail.com>
2025-09-20 12:17:19 +0600

    all in complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Suspense } from 'react';
 import './App.css'
-import Countries from "./components/Countries/Countries"
+import Countries from "./components/Countries/Countries";
+
+const countriesPromis = fetch("https://openapi.programming-hero.com/api/all")
+  .then(res => res.json())
 
 function App() {
 
   return (
     <>
-      <h1>React World on the go.</h1>
-      <Countries></Countries>
+      <Suspense fallback={<h3>On the go loadding...</h3>}>
+        <Countries countriesPromis={countriesPromis}></Countries>
+      </Suspense>
     </>
   )
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import { use, useState } from "react";
+import React from "react";
+import Country from "../Country/Country";
 
-const countries = () => {
+const countries = ({ countriesPromis }) => {
+  const [visitedCountries, setVisitedCountires] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([])
+
+
+  const handleVisitedCountries = (country) => {
+    console.log("Total Visited Countries", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountires(newVisitedCountries);
+  };
+
+  const handleVisitedFlag = (flag) => {
+    const newVisitedFlags = [...visitedFlags, flag]
+    setVisitedFlags(newVisitedFlags)
+  }
+
+  const countriesData = use(countriesPromis);
+  const countries = countriesData.countries;
+  // console.log(countries);
   return (
     <div>
-      <h1>I am countries here</h1>
+      <h1>I am countries here: {countries.length}</h1>
+      <h3>Total Country Visited: {visitedCountries.length}</h3>
+      <h3>Total Flags Visited: {visitedFlags.length}</h3>
+      <ol>
+        {
+          visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+        }
+      </ol>
+      <div className="visited-flag-container">
+        {
+          visitedFlags.map((flag, index) => <img key={index} src={flag}/>)
+        }
+      </div>
+      <div className="parent-card">
+        {countries.map((country) => (
+          <Country
+            country = {country}
+            key = {country.cca3.cca3}
+            handleVisitedCountries = {handleVisitedCountries}
+            handleVisitedFlag = {handleVisitedFlag}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
